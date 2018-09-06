6d0def639a701a9eb6b974e14a7632929ffce9d7
Paktalin <likorn@ttu.ee>
2018-09-06 20:10:02 +0200

    Removed views initialization in Kotlin SignInActivity

--- app/src/main/java/com/paktalin/vocabularynotebook/SignInActivity.kt
+++ app/src/main/java/com/paktalin/vocabularynotebook/SignInActivity.kt
@@ -5,20 +5,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.signin.SignIn
 
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
-
-    private var mEmailEt: EditText? = null
-    private var mPasswordEt: EditText? = null
-    private var mSignInBtn: Button? = null
-    private var mSignUpBtn: Button? = null
 
     private var mAuth: FirebaseAuth? = null
 
@@ -26,10 +19,9 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        initializeViews()
         mAuth = FirebaseAuth.getInstance()
-        mSignInBtn!!.setOnClickListener({ signIn() })
-        mSignUpBtn!!.setOnClickListener({ signUp() })
+        btnSignIn!!.setOnClickListener({ signIn() })
+        btnSignUp!!.setOnClickListener({ signUp() })
     }
 
     override fun onStart() {
@@ -38,16 +30,9 @@ class SignInActivity : AppCompatActivity() {
         if (currentUser != null) startUserActivity()
     }
 
-    private fun initializeViews() {
-        mEmailEt = findViewById(R.id.email)
-        mPasswordEt = findViewById(R.id.password)
-        mSignInBtn = findViewById(R.id.btn_sign_in)
-        mSignUpBtn = findViewById(R.id.btn_sign_up)
-    }
-
     private fun signIn() {
-        val email = mEmailEt!!.text.toString()
-        val password = mPasswordEt!!.text.toString()
+        val email = etEmail!!.text.toString()
+        val password = etPassword!!.text.toString()
 
         if (fieldsNotEmpty(email, password)) {
             mAuth!!.signInWithEmailAndPassword(email, password)
@@ -64,8 +49,8 @@ class SignInActivity : AppCompatActivity() {
     }
 
     private fun signUp() {
-        val email = mEmailEt!!.text.toString()
-        val password = mPasswordEt!!.text.toString()
+        val email = etEmail!!.text.toString()
+        val password = etPassword!!.text.toString()
 
         if (fieldsNotEmpty(email, password)) {
             mAuth!!.createUserWithEmailAndPassword(email, password)
