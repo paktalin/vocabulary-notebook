ae7ee12be60bd486a3c9d6f2db8cf2c513290c0b
Paktalin <likorn@ttu.ee>
2018-09-08 01:17:56 +0200

    Removed test fields from VocabularyFragment

--- app/src/main/java/com/paktalin/vocabularynotebook/activities/VocabularyFragment.kt
+++ app/src/main/java/com/paktalin/vocabularynotebook/activities/VocabularyFragment.kt
@@ -1,11 +1,9 @@
 package com.paktalin.vocabularynotebook.activities
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,13 +15,13 @@ import com.paktalin.vocabularynotebook.R
 import com.paktalin.vocabularynotebook.VocabularyAdapter
 import com.paktalin.vocabularynotebook.pojo.WordItemPojo
 import kotlinx.android.synthetic.main.fragment_vocabulary.*
-import java.util.*
 
 class VocabularyFragment : Fragment() {
     companion object {
         private val TAG = "VN/" + VocabularyFragment::class.simpleName
         private const val VOCABULARIES = "vocabularies"
         private const val WORDS = "words"
+        private const val USERS = "users"
     }
 
     private lateinit var userDocument: DocumentReference
@@ -37,8 +35,7 @@ class VocabularyFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         fabAddWord.setOnClickListener( { addWord() } )
-        extractUserDocument()
-        printUserData()
+        retrieveData()
     }
 
     override fun onDestroy() {
@@ -46,10 +43,14 @@ class VocabularyFragment : Fragment() {
         FirebaseAuth.getInstance().signOut()
     }
 
-    private fun extractUserDocument() {
+    private fun retrieveData() {
         val userId = FirebaseAuth.getInstance().currentUser!!.uid
-        Log.d(TAG, "retrieved userId: $userId")
-        userDocument = db.collection("users").document(userId)
+        userDocument = db.collection(USERS).document(userId)
+
+        userDocument.get().addOnSuccessListener { task ->
+            setVocabularyId(task)
+            retrieveVocabularyData()
+        }
     }
 
     private fun setVocabularyId(task: DocumentSnapshot) {
@@ -58,33 +59,8 @@ class VocabularyFragment : Fragment() {
         vocabulary = db.collection(VOCABULARIES).document(vocabularies[0].id)
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun printUserData() {
-        userDocument.get().addOnSuccessListener { task ->
-            val email = task.get("email").toString()
-            tvUserData.text = email
-
-            setVocabularyId(task)
-            retrieveVocabularyData()
-        }
-    }
-
     private fun retrieveVocabularyData() {
         //todo if only one vocabulary exists, open it
-        vocabulary.get().addOnSuccessListener { task ->
-            val vocabularyTitle = task.get("title").toString()
-            tvUserData.append("\n\nvocabularies:\n$vocabularyTitle")
-        }
-        retrieveWordsFromVocabulary()
-    }
-
-    private fun addWord() {
-        val addWordIntent = Intent(activity, AddWordActivity::class.java)
-        addWordIntent.putExtra("vocabularyId", vocabulary.id)
-        startActivity(addWordIntent)
-    }
-
-    private fun retrieveWordsFromVocabulary() {
         val mLayoutManager = LinearLayoutManager(activity)
         recyclerView.layoutManager = mLayoutManager
         recyclerView.setHasFixedSize(true)
@@ -101,5 +77,11 @@ class VocabularyFragment : Fragment() {
             val adapter = VocabularyAdapter(wordItems)
             recyclerView.adapter = adapter
         }
+    }
+
+    private fun addWord() {
+        val addWordIntent = Intent(activity, AddWordActivity::class.java)
+        addWordIntent.putExtra("vocabularyId", vocabulary.id)
+        startActivity(addWordIntent)
     }
 }
